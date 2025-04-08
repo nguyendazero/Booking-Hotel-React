80a60e2e93f322cd866548a59187c8cfa05bb4ee
Nguyen Da Da <nguyendazero@gmail.com>
2025-04-08 10:47:53 +0700

    history booking

--- src/pages/BookingHistory.jsx
+++ src/pages/BookingHistory.jsx
@@ -3,11 +3,13 @@ import useFetch from "../hooks/useFetch";
 import LoadingSpinner from "../components/Common/LoadingSpinner";
 import { useSelector } from "react-redux";
 import BookingHistory from "../components/BookingHistory";
+import { Link } from "react-router-dom";
+import { Button, Result } from "antd"; // Nhập Result và Button từ antd
 
 function BookingHistoryPage() {
   // Get token from Redux
   const token = useSelector((state) => state.auth.token);
-  
+
   const {
     data: bookingsHistory,
     loading: bookingsHistoryLoading,
@@ -29,7 +31,23 @@ function BookingHistoryPage() {
   if (bookingsHistoryLoading) return <LoadingSpinner />;
   if (bookingsHistoryError)
     return <p>Error loading check-in details: {bookingsHistoryError}</p>;
-  if (!bookingsHistory || bookingsHistory.length === 0) return <p>No bookings found</p>;
+  if (!bookingsHistory || bookingsHistory.length === 0)
+    return (
+      <div className="mt-5 mb-5">
+        <Result
+          status="info"
+          title="You have not booked any hotel in history"
+          subTitle="It looks like there are no bookings in your account. Please make a reservation to see it here."
+          extra={
+            <Link to="/hotels">
+              <Button type="primary" key="newBooking">
+                Booking now
+              </Button>
+            </Link>
+          }
+        />
+      </div>
+    );
 
   return (
     <>
--- src/pages/Checkin.jsx
+++ src/pages/Checkin.jsx
@@ -3,11 +3,13 @@ import CheckinHotel from "../components/CheckinHotel";
 import useFetch from "../hooks/useFetch";
 import LoadingSpinner from "../components/Common/LoadingSpinner";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { Button, Result } from "antd"; // Nhập Result và Button từ antd
 
 function CheckinPage() {
   // Get token from Redux
   const token = useSelector((state) => state.auth.token);
-  
+
   const {
     data: bookings,
     loading: bookingLoading,
@@ -29,7 +31,23 @@ function CheckinPage() {
   if (bookingLoading) return <LoadingSpinner />;
   if (bookingError)
     return <p>Error loading check-in details: {bookingError}</p>;
-  if (!bookings || bookings.length === 0) return <p>No bookings found</p>;
+  if (!bookings || bookings.length === 0)
+    return (
+      <div className="mt-5 mb-5">
+        <Result
+          status="info"
+          title="No Bookings Found"
+          subTitle="It looks like there are no bookings in your account. Please make a reservation to see it here."
+          extra={
+            <Link to="/hotels">
+              <Button type="primary" key="newBooking">
+                Find Hotels
+              </Button>
+            </Link>
+          }
+        />
+      </div>
+    );
 
   return (
     <>
