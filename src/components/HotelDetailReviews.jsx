ee17ae1d9619577e4e18e1e1c4b3b2381a6eb034
Nguyen Da Da <nguyendazero@gmail.com>
2025-04-08 17:18:58 +0700

    add review

--- src/components/HotelDetailReviews.jsx
+++ src/components/HotelDetailReviews.jsx
@@ -83,7 +83,7 @@ const HotelDetailReviews = ({
                 className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4"
                 role="alert"
               >
-                <strong className="font-bold">Error!</strong>
+                <strong className="font-bold">Error! </strong>
                 <span className="block sm:inline">{reviewError}</span>
               </div>
             )}
