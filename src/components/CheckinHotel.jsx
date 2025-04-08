5fbc6c21ff953447684806680f8ca52e3197a402
Nguyen Da Da <nguyendazero@gmail.com>
2025-04-08 17:33:13 +0700

    add review

--- src/components/CheckinHotel.jsx
+++ src/components/CheckinHotel.jsx
@@ -10,6 +10,11 @@ const CheckinHotel = ({ bookings, onBookingCancelled }) => {
   const token = useSelector((state) => state.auth.token);
   const { putData, loading: putLoading, error: putError } = usePut();
 
+  // Filter bookings to show only those with status "PENDING" or "CONFIRMED"
+  const upcomingCheckins = bookings.filter(
+    (booking) => booking.status === "PENDING" || booking.status === "CONFIRMED" || booking.status === "CANCELLED"
+  );
+
   const handleCancelBooking = async (bookingId) => {
     setLoadingBookingId(bookingId);
     const url = `http://localhost:8080/api/v1/user/booking/${bookingId}/cancel`;
@@ -61,76 +66,82 @@ const CheckinHotel = ({ bookings, onBookingCancelled }) => {
       </h1>
 
       <div className="space-y-6">
-        {bookings.map((booking) => (
-          <div
-            key={booking.id}
-            className="flex border border-gray-300 rounded-md bg-gray-50 shadow-sm p-5 hover:bg-gray-100 hover:shadow-lg transition-all duration-300 transform hover:scale-105"
-          >
-            <img
-              src={booking.hotel.highLightImageUrl || "default_image_url.jpg"}
-              alt={booking.hotel.name}
-              className="w-70 h-50 rounded-md mr-5 object-cover"
-            />
-            <div className="flex-1">
-              <h3 className="text-2xl font-bold text-blue-800 hover:text-blue-600 transition duration-200">
-                {booking.hotel.name}
-              </h3>
-              <p className="text-gray-500 text-sm italic mb-1">
-                Location: {booking.hotel.streetAddress}
-              </p>
-              <p className="text-lg font-semibold text-gray-800 mb-1">
-                Price:{" "}
-                <span className="font-bold text-green-700">
-                  {booking.totalPrice} $
-                </span>
-              </p>
-              <p className="text-gray-500 text-sm mb-1">
-                Status:{" "}
-                <span
-                  className={`font-semibold ${
-                    booking.status === "CONFIRMED"
-                      ? "text-green-600"
-                      : "text-red-600"
-                  }`}
-                >
-                  {booking.status}
-                </span>
-              </p>
-              <p className="text-gray-500 text-xs">
-                Start Date:{" "}
-                <span className="font-medium text-gray-700">
-                  {formatDate(booking.startDate)}
-                </span>
-              </p>
-              <p className="text-gray-500 text-xs">
-                End Date:{" "}
-                <span className="font-medium text-gray-700">
-                  {formatDate(booking.endDate)}
-                </span>
-              </p>
-              <div className="mt-5">
-                <Link to={`/hotels/${booking.hotel.id}`}>
-                  <Button color="purple" variant="solid" className="mr-1.5">
-                    View
-                  </Button>
-                </Link>
-                {booking.status !== "CANCELLED" && (
-                  <Button
-                    color="danger"
-                    variant="solid"
-                    onClick={() => handleButtonClick(booking.id)}
-                    loading={loadingBookingId === booking.id || putLoading}
-                    disabled={loadingBookingId === booking.id || putLoading}
+        {upcomingCheckins.length === 0 ? (
+          <p className="text-gray-600 text-center text-lg">
+            No upcoming check-ins found.
+          </p>
+        ) : (
+          upcomingCheckins.map((booking) => (
+            <div
+              key={booking.id}
+              className="flex border border-gray-300 rounded-md bg-gray-50 shadow-sm p-5 hover:bg-gray-100 hover:shadow-lg transition-all duration-300 transform hover:scale-105"
+            >
+              <img
+                src={booking.hotel.highLightImageUrl || "default_image_url.jpg"}
+                alt={booking.hotel.name}
+                className="w-70 h-50 rounded-md mr-5 object-cover"
+              />
+              <div className="flex-1">
+                <h3 className="text-2xl font-bold text-blue-800 hover:text-blue-600 transition duration-200">
+                  {booking.hotel.name}
+                </h3>
+                <p className="text-gray-500 text-sm italic mb-1">
+                  Location: {booking.hotel.streetAddress}
+                </p>
+                <p className="text-lg font-semibold text-gray-800 mb-1">
+                  Price:{" "}
+                  <span className="font-bold text-green-700">
+                    {booking.totalPrice} $
+                  </span>
+                </p>
+                <p className="text-gray-500 text-sm mb-1">
+                  Status:{" "}
+                  <span
+                    className={`font-semibold ${
+                      booking.status === "CONFIRMED"
+                        ? "text-green-600"
+                        : "text-red-600"
+                    }`}
                   >
-                    {loadingBookingId === booking.id || putLoading
-                      ? "Cancelling..."
-                      : "Cancel"}
-                  </Button>
-                )}
+                    {booking.status}
+                  </span>
+                </p>
+                <p className="text-gray-500 text-xs">
+                  Start Date:{" "}
+                  <span className="font-medium text-gray-700">
+                    {formatDate(booking.startDate)}
+                  </span>
+                </p>
+                <p className="text-gray-500 text-xs">
+                  End Date:{" "}
+                  <span className="font-medium text-gray-700">
+                    {formatDate(booking.endDate)}
+                  </span>
+                </p>
+                <div className="mt-5">
+                  <Link to={`/hotels/${booking.hotel.id}`}>
+                    <Button color="purple" variant="solid" className="mr-1.5">
+                      View
+                    </Button>
+                  </Link>
+                  {booking.status !== "CANCELLED" && (
+                    <Button
+                      color="danger"
+                      variant="solid"
+                      onClick={() => handleButtonClick(booking.id)}
+                      loading={loadingBookingId === booking.id || putLoading}
+                      disabled={loadingBookingId === booking.id || putLoading}
+                    >
+                      {loadingBookingId === booking.id || putLoading
+                        ? "Cancelling..."
+                        : "Cancel"}
+                    </Button>
+                  )}
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))
+        )}
       </div>
     </div>
   );
