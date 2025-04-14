d3c5accb99e25932f5cfd78661bdeed6ae540876
Nguyen Da Da <nguyendazero@gmail.com>
2025-04-14 14:27:41 +0700

    move modal add hotel to file invidial

--- src/components/AddHotelModal.jsx
+++ src/components/AddHotelModal.jsx
@@ -77,7 +77,9 @@ const AddHotelModal = ({
               Price Per Day
             </span>
           }
-          rules={[{ required: true, message: "Please enter the price per day" }]}
+          rules={[
+            { required: true, message: "Please enter the price per day" },
+          ]}
         >
           <InputNumber
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
