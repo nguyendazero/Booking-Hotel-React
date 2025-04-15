6ca32a5e195e340434fad5eecdc4edf7b0abd0a5
Nguyen Da Da <nguyendazero@gmail.com>
2025-04-15 14:59:31 +0700

    move sidebar to component invidual

--- src/components/AdminSidebar.jsx
+++ src/components/AdminSidebar.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Layout, Menu } from "antd";
+import { Link } from "react-router-dom";
 import {
-  PieChartOutlined,
-  FileOutlined,
-  TeamOutlined,
-  UserOutlined,
-} from "@ant-design/icons";
-import { Link } from "react-router-dom"; // Nếu bạn muốn dùng Link cho điều hướng
+  PieChart,
+  Users,
+  Building,
+  UserPlus,
+  CalendarCheck,
+} from "lucide-react";
 
 const { Sider } = Layout;
 const { SubMenu } = Menu;
@@ -28,29 +29,26 @@ function AdminSidebar({ collapsed, onCollapse }) {
         </h1>
       </div>
       <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
-        <Menu.Item key="1" icon={<PieChartOutlined />}>
-          Dashboard
-          {/* Nếu bạn muốn dùng Link để điều hướng */}
-          {/* <Link to="/admin" /> */}
+        <Menu.Item key="1" icon={<PieChart />}>
+          <Link to="/admin">Dashboard</Link>
         </Menu.Item>
-        <SubMenu key="sub1" icon={<UserOutlined />} title="Users">
-          <Menu.Item key="3">List Users</Menu.Item>
-          {/* <Link to="/admin/users" /> */}
-          <Menu.Item key="4">Add User</Menu.Item>
-          {/* <Link to="/admin/users/add" /> */}
+        <SubMenu key="sub1" icon={<Users />} title="Users">
+          <Menu.Item key="3">
+            <Link to="/admin/users">List Users</Link>
+          </Menu.Item>
         </SubMenu>
-        <SubMenu key="sub2" icon={<TeamOutlined />} title="Hotels">
-          <Menu.Item key="6">List Hotels</Menu.Item>
-          {/* <Link to="/admin/hotels" /> */}
-          <Menu.Item key="8">Add Hotel</Menu.Item>
-          {/* <Link to="/admin/hotels/add" /> */}
+        <SubMenu key="sub2" icon={<Building />} title="Hotels">
+          <Menu.Item key="5">
+            <Link to="/admin/hotels">List Hotels</Link>
+          </Menu.Item>
         </SubMenu>
-        <SubMenu key="sub3" icon={<TeamOutlined />} title="Owner Registration">
-          <Menu.Item key="6">List Registration</Menu.Item>
+        <SubMenu key="sub3" icon={<UserPlus />} title="Registrations">
+          <Menu.Item key="7">
+            <Link to="/admin/registration">List Registration</Link>
+          </Menu.Item>
         </SubMenu>
-        <Menu.Item key="9" icon={<FileOutlined />}>
-          Bookings
-          {/* <Link to="/admin/bookings" /> */}
+        <Menu.Item key="9" icon={<CalendarCheck />}>
+          <Link to="/admin/bookings">Bookings</Link>
         </Menu.Item>
       </Menu>
     </Sider>
